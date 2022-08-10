f3b4060dcb6d674d106eca26223e2bcb5e4b271a
LuoMiao <1084459061@qq.com>
2022-08-10 11:18:43 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    App
+    <p>{{$store.state.username}}</p>
+    <p>{{$store.getters['newName']}}</p>
+    <button @click="mutationsFn">mutationsFn</button>
+  </div>
 </template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    const store = useStore()
+    console.log(store.state.username)
+    console.log(store.getters.newName)
+    const mutationsFn = () => {
+      store.dispatch('updateName')
+    }
+    return { mutationsFn }
+  }
+}
+</script>
 
 <style lang="less">
 #app {
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    username: '潘先源'
   },
   getters: {
+    newName (state) {
+      return state.username + '!!!'
+    }
   },
   mutations: {
+    updateName (state) {
+      state.username = '源总'
+    }
   },
   actions: {
+    updateName (context) {
+      // 发请求
+      setTimeout(() => {
+        context.commit('updateName')
+      }, 2000)
+    }
   },
   modules: {
   }
